80da93344caf563526a5b2fb88cbd774ca44be89
ianmat55 <ianmat55@gmail.com>
2023-10-02 06:56:39 -1000

    added transparent background in editor

--- lua/imats/plugins-setup.lua
+++ lua/imats/plugins-setup.lua
@@ -67,6 +67,9 @@ return packer.startup(function(use)
     use('L3MON4D3/LuaSnip')
     use('saadparwaiz1/cmp_luasnip')
     use('rafamadriz/friendly-snippets')
+    
+    -- transparent editor
+    use('xiyaowong/transparent.nvim')
 
     -- lsp
 	use 'williamboman/mason.nvim'
