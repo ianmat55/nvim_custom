96d49b035436360f48914b2f07e18035d00d55f3
ianmat55 <ianmat55@gmail.com>
2023-10-08 20:27:26 -1000

    more changes

--- lua/imats/plugins/lsp/lspconfig.lua
+++ lua/imats/plugins/lsp/lspconfig.lua
@@ -8,6 +8,12 @@ if not cmp_nvim_lsp_status then
     return
 end
 
+-- import typescript plugin safely
+local typescript_setup, typescript = pcall(require, "typescript")
+if not typescript_setup then
+  return
+end
+
 local keymap = vim.keymap
 
 -- enable keybinds for available lsp server
@@ -28,14 +34,58 @@ local on_attach = function(client, bufnr)
     keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
     keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
     keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+
+    -- typescript specific keymaps (e.g. rename file and update imports)
+    if client.name == "ts-standard" then
+        keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
+        keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
+        keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
+    end
 end
 
 -- used to enable autocompletion
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+-- Change the Diagnostic symbols in the sign column (gutter)
+-- (not in youtube nvim video)
+local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 lspconfig['pyright'].setup({
     capabilities = capabilities,
     on_attach = on_attach,
     filetypes = {'python'}
+})
+
+-- configure typescript server with plugin
+typescript.setup({
+  server = {
+    capabilities = capabilities,
+    on_attach = on_attach,
+  },
+})
+
+-- configure lua server (with special settings)
+lspconfig["lua_ls"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = { -- custom settings for lua
+    Lua = {
+      -- make the language server recognize "vim" global
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        -- make language server aware of runtime files
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
+    },
+  },
 })
 
