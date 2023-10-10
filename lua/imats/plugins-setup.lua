0fca2b5f2288c202a6a0bc44939ed6667e0214d9
ianmat55 <ianmat55@gmail.com>
2023-10-09 17:34:02 -1000

    tree sitter syntax highlighting and git plugin

--- lua/imats/plugins-setup.lua
+++ lua/imats/plugins-setup.lua
@@ -78,16 +78,36 @@ return packer.startup(function(use)
     -- configures lsp servers
 	use 'neovim/nvim-lspconfig'
     use 'hrsh7th/cmp-nvim-lsp'
-    use { 'glepnir/lspsaga.nvim', branch = 'main' }
+    use({
+        "glepnir/lspsaga.nvim",
+        branch = "main",
+        requires = {
+            { "nvim-tree/nvim-web-devicons" },
+            { "nvim-treesitter/nvim-treesitter" },
+        },
+    }) -- enhanced lsp uis
+    -- use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
     use 'onsails/lspkind.nvim'
 
     -- formatting & linting
     use 'jose-elias-alvarez/null-ls.nvim'
     use 'jayp0521/mason-null-ls.nvim'
 
+    -- treesitter configuration
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+            ts_update()
+        end,
+    })
+
     -- auto closing
     use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-    --use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+    use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+
+    -- git integration
+    use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
     -- don't know why we need this, but docs say we do
     if packer_bootstrap then
--- lua/imats/plugins/lsp/null-ls.lua
+++ lua/imats/plugins/lsp/null-ls.lua
@@ -1,26 +1,33 @@
-local setup, null_ls = pcall(require, 'null-ls')
+-- import null-ls plugin safely
+local setup, null_ls = pcall(require, "null-ls")
 if not setup then
-    return
+  return
 end
 
-local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
+-- for conciseness
+local formatting = null_ls.builtins.formatting -- to setup formatters
+local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+-- to setup format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+-- configure null_ls
 null_ls.setup({
-    sources = {
-        formatting.prettier,
-        formatting.stylua,
-        diagnostics.eslint_d.with({ -- js/ts linter
-            -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-            condition = function(utils)
-                return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-            end,
-        }),
-    },
-    -- configure format on save
-    on_attach = function(current_client, bufnr)
+  -- setup formatters & linters
+  sources = {
+    --  to disable file types use
+    --  "formatting.prettier.with({disabled_filetypes = {}})" (see null-ls docs)
+    formatting.prettier, -- js/ts formatter
+    formatting.stylua, -- lua formatter
+    diagnostics.eslint_d.with({ -- js/ts linter
+      -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+      condition = function(utils)
+        return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+      end,
+    }),
+  },
+  -- configure format on save
+  on_attach = function(current_client, bufnr)
     if current_client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
